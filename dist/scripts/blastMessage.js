95b340960c0937a6e5c80fbb9b4343156289ef10
septian <septianibnyohan@gmail.com>
2022-12-24 13:15:17 +0700

    implement toDataURL

--- dist/scripts/blastMessage.js
+++ dist/scripts/blastMessage.js
@@ -1,12 +1,30 @@
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 var fileUpload = document.getElementById("phone_number");
   var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
+
+  var minDelay = parseInt(document.getElementsByName("minDelay")[0].value) * 1000;
+  var maxDelay = parseInt(document.getElementsByName("maxDelay")[0].value) * 1000;
+
+  console.log(minDelay);
+  console.log(maxDelay);
+  //return;
+
+  
 
   if (regex.test(fileUpload.value.toLowerCase())) {
     if (typeof (FileReader) != "undefined") {
       var reader = new FileReader();
-      reader.onload = function (e) {
+      reader.onload = async function (e) {
         var rows = e.target.result.split("\n");
         for (var i = 1; i < rows.length; i++) {
+          var delay = getRndInteger(minDelay, maxDelay);
           var cells = rows[i].split(",");
           if (cells.length > 1) {
               var panggilan = cells[0];
@@ -25,7 +43,12 @@ var fileUpload = document.getElementById("phone_number");
 
               console.log(message);
               var chat_id = phone.trim() + "@c.us";
+
+              console.log(`send Message to ${chat_id}`)
               window.WAPI.sendMessage(chat_id,message);
+              console.log(`wait ${delay} ms`);
+              
+              await sleep(delay);
           }
         }
       }
